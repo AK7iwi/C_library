9ddebb01e8f18ef2c6341684930c3d046fe28320
Michael FELDMAN <mfeldman@e3r7p1.clusters.42paris.fr>
2022-05-16 06:21:23 +0200

    test essai

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 03:04:31 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/16 06:02:04 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/16 06:17:57 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@
 
 char *ft_substr(char const *s, unsigned int start,size_t len)
 {
+    /*
     char *dest;
    
     dest = malloc(sizeof (char) * (len + 1));
     if (!dest)
         return(NULL);
-    /*
+    
+    
     if (len <= start)
         return(NULL);
     while (len >= start)
@@ -30,8 +32,10 @@ char *ft_substr(char const *s, unsigned int start,size_t len)
         dest[len - 1] = s[len + start - 2];
         len--;
     }
-    */
+    
     return(dest);
+    */
+    printf("%s", "oui");
 }
 
 
