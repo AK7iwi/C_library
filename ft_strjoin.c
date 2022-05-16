2cbc6e4ac7cd674298ae5c8b5440c2353aaf0409
Michael FELDMAN <mfeldman@e3r7p1.clusters.42paris.fr>
2022-05-16 04:33:46 +0200

    fct

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/16 03:04:31 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/16 04:31:52 by mfeldman         ###   ########.fr       */
+/*   Created: 2022/05/16 03:36:12 by mfeldman          #+#    #+#             */
+/*   Updated: 2022/05/16 03:58:08 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdLib.h>
-
-
-char *ft_substr(char const *s, unsigned int start,size_t len)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-    char * dest;
-    
-    dest = malloc(sizeof (char) * (len + 1));
-    return(dest)
-
+        c
 }
-
-int main()
-{
-    
-}
-
-
-
