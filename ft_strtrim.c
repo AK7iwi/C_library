b9c5cc87c9330165bcf0858f13c7600ae3866e44
Miki Le Kiwi <michael.feldman.ca@gmail.com>
2022-05-18 09:50:37 +0200

    split + strtrm v2

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,42 +6,44 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 02:48:46 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/18 08:25:25 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/18 09:49:39 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char *ft_strtrim(char const *s1, char const *set)
+int		ft_set(char c, char const *set)
 {
-    char *s2;
-    size_t i;
-    size_t j;
-    size_t k;
-    
-    
-    s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) +1 ));
-    i = 0;
-    k = 0;
-    if (!s2)
-        return(NULL);      
-    while(s1[i]) 
-    {
-        j = 0;
-        while(set[j])
-        {
-            if(s1[i] != set[j])
-                j++;            
-        }
-        i++;
-        
-    } 
-    while(s1[i] && s1[i] != set[j])
-    {
-        s2[k] = s1[i];
-        k++;
-        i++;
-    }
-    s2[k] = '\0';
-    return(s2);
-    
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i++] == c)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+
+	i = 0;
+	start = 0;
+	if (s1 == 0 || set == 0)
+		return (NULL);
+	end = ft_strlen(s1);
+	while (s1[start] && ft_set(s1[start], set))
+		start++;
+	while (end > start && ft_set(s1[end - 1], set))
+		end--;
+	if (!(str = (char *)malloc(sizeof(char) * (end - start + 1))))
+		return (NULL);
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = '\0';
+	return (str);
 }
