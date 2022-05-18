2dba78714ad203f3cb660e37bb3fe7b8c8fe49b7
Michael FELDMAN <mfeldman@e3r7p1.clusters.42paris.fr>
2022-05-18 05:25:53 +0200

    debut strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 02:48:46 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/18 03:30:19 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/18 05:25:32 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,25 @@ char *ft_strtrim(char const *s1, char const *set)
     size_t k;
     
     
-    s2 = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+    s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) +1 ));
     i = 0;
     k = 0;
     if (!s2)
-        return(NULL);       
-    while(s1[i])
+        return(NULL);      
+    while(s1[i]) 
     {
         j = 0;
-        while(set[j])
-        {
-            if(s1[i] != set[j])
-            {
-                s2[k] = set[j];
+        while(set[j] && s1[i] == set[j])
                 j++;
-                k++;
-            }
-        }
+                i
+        i++;
+    } 
+    while(s1[i] && s1[i] != set[j])
+    {
+        s2[k] = s1[i];
+        k++;
         i++;
     }
+    s2[k] = '\0';
     return(s2);
-    
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 09:41:57 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/18 02:42:51 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/18 04:38:09 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,5 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char    *ft_substr(char const *s, unsigned int start,size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
 #endif
