9d3242416ba129f26b5cd08a41b3a6a853272f36
Miki Le Kiwi <michael.feldman.ca@gmail.com>
2022-05-19 00:58:26 +0200

    ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,20 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 00:40:19 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/19 00:41:35 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/19 00:57:09 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    
+	t_list	*dst;
+
+	dst = malloc(sizeof(t_list));
+	if (!dst)
+		return (NULL);
+	dst->content = content;
+	dst->next = NULL;
+	return (dst);
 }
