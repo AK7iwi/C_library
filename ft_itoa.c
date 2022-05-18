bdbf03a023fe9a48cb2e99b23b8eab7c03ab732a
Miki Le Kiwi <michael.feldman.ca@gmail.com>
2022-05-18 11:32:53 +0200

    ioa v1

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,57 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:32:39 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/18 10:43:10 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/18 11:31:48 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char *ft_itoa(int n)
+static int	ft_len(long long nb)
 {
-	char *dst;
-	dst = char malloc(sizeof(char))
-	return(dst);
+	long long	n;
+	int			count;
+
+	n = nb;
+	count = 0;
+	if (n == 0)
+		count++;
+	if (n < 0)
+	{
+		count++;
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		count++;
+		n /= 10;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int nb)
+{
+	char		*dest;
+	long long	n;
+	int			size;
+
+	n = nb;
+	size = ft_len(n);
+	dest = malloc(sizeof(char) * (size + 1));
+	if (!dest)
+		return (NULL);
+	dest[size--] = '\0';
+	if (n == 0)
+		dest[size] = '0';
+	if (n < 0)
+	{
+		dest[0] = '-';
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		dest[size] = ((n % 10) + '0');
+		n /= 10;
+		size--;
+	}
+	return (dest);
 }
