adc2cdb06d50ccc11e06fd2755f7b69662147f14
Miki Le Kiwi <michael.feldman.ca@gmail.com>
2022-05-18 10:43:36 +0200

    Commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 09:41:57 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/18 09:08:05 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/18 10:37:54 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,5 @@ char    *ft_substr(char const *s, unsigned int start,size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *str, char c);
+char 	*ft_itoa(int n);
 #endif
