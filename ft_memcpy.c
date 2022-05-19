f8aa68ee697ae5e741be0f49c5b70ae82754c250
Miki Le Kiwi <michael.feldman.ca@gmail.com>
2022-05-19 08:33:30 +0200

    Protection

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 04:07:11 by mfeldman          #+#    #+#             */
-/*   Updated: 2022/05/19 07:19:35 by mfeldman         ###   ########.fr       */
+/*   Updated: 2022/05/19 08:33:25 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	size_t i;
 
 	i = 0;
-
+	if (!dest && !src)
+                return (NULL);
 	while (i < n)
 	{
 		(*(unsigned char *)(dest + i)) = (*(unsigned char *)(src + i));
